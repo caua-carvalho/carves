c022237a99f459bd543a64a42e987d2204ecee78
caua-carvalho <caua.carma033@gmail.com>
2024-11-14 01:46:00 -0300

    alteração de design do index

--- componentes/navbar.php
+++ componentes/navbar.php
@@ -1,15 +1,17 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark d-flex justify-content-center">
-    <a class="navbar-brand" href="..\index.php">
-        <img src="./imagens/calves.png" alt="Logo" class="navbar-logo"> <!-- Logo -->
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ms-auto">
-            <li class="nav-item"><a class="nav-link" href="cadastro\cadastro.php">Cadastro</a></li>
-            <li class="nav-item"><a class="nav-link" href="#form">Editar</a></li>
-        </ul>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container d-flex justify-content-center">
+        <a class="navbar-brand" href="../index.php">
+            <img src="./imagens/calves.png" alt="Logo" class="navbar-logo">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link text-center" href="cadastro/cadastro.php">Cadastro</a></li>
+                <li class="nav-item"><a class="nav-link text-center" href="#form">Editar</a></li>
+            </ul>
+        </div>
     </div>
 </nav>
